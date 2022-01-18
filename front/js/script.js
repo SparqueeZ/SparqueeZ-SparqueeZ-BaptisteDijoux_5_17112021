8f0b07bc055fd2df59d0f7daf59683385579eeab
Baptiste <baptiste.dijouxn8@gmail.com>
2022-01-18 14:45:57 +0400

    Ajout des produits sur la page d'accueil grâce au fetch

---
+++ front/js/script.js
@@ -0,0 +1,27 @@
+let kanapData = [];
+
+const fetchKanap = async () => {
+    await fetch("http://localhost:3000/api/products")
+    .then ((res) => res.json())
+    .then ((promise) => {
+        kanapData = promise;
+        console.log(kanapData);
+    })
+};
+
+const kanapDisplay = async () => {
+    await fetchKanap();
+
+    document.getElementById("items")
+    .innerHTML = kanapData.map((kanap) => `
+    <a href="./product.html?id=${kanap._id}">
+    <article>
+        <h3 class="productName" >${kanap.name} </h3>
+        <img src="${kanap.imageUrl}" alt="image test">
+        <p class="productDescription">${kanap.description}</p>  
+    </article>`
+    )
+    .join(""); //enlever les virgules
+};
+
+kanapDisplay();
